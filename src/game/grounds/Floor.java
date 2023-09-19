e11ac6f91da7fcc4e8ef41065c5b485975b58769
dpar0036 <dpar0036@student.monash.edu>
2023-09-19 08:10:46 +0000

    Replace Floor.java

--- src/game/grounds/Floor.java
+++ src/game/grounds/Floor.java
@@ -1,16 +1,37 @@
 package game.grounds;
 
+import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
+import game.Status;
 
 /**
  * A class that represents the floor inside a building.
  * Created by:
  * @author Riordan D. Alfredo
  * Modified by:
- *
+ * Lim Hung Xuan
  */
 public class Floor extends Ground {
+
+    /**
+     * Constructor for the Floor class.
+     */
     public Floor() {
         super('_');
+    }
+
+    /**
+     * Checks whether an actor can enter the floor.
+     *
+     * @param actor The actor attempting to enter the floor.
+     * @return True if the actor has the capability "HOSTILE_TO_ENEMY," allowing them to enter; otherwise, false.
+     */
+    public boolean canActorEnter(Actor actor) {
+        if (actor.hasCapability(Status.HOSTILE_TO_ENEMY)) {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
