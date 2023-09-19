7977927226f5c8a708dce3061be5bd23f3bf1b54
dpar0036 <dpar0036@student.monash.edu>
2023-09-19 08:10:36 +0000

    Replace Dirt.java

--- src/game/grounds/Dirt.java
+++ src/game/grounds/Dirt.java
@@ -7,10 +7,13 @@ import edu.monash.fit2099.engine.positions.Ground;
  * Created by:
  * @author Riordan D. Alfredo
  * Modified by:
- *
+ * Lim Hung Xuan
  */
 public class Dirt extends Ground {
 
+    /**
+     * Constructor for the Dirt class.
+     */
     public Dirt() {
         super('.');
     }
