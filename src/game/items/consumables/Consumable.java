7151e52334cb5660366b4d2befb12103917cec9c
dpar0036 <dpar0036@student.monash.edu>
2023-09-19 08:12:05 +0000

    Replace Consumable.java

--- src/game/items/consumables/Consumable.java
+++ src/game/items/consumables/Consumable.java
@@ -1,4 +1,18 @@
 package game.items.consumables;
 
-public class Consumable {
+import edu.monash.fit2099.engine.actors.Actor;
+
+/**
+ * An interface representing a consumable item that can be used by an actor.
+ */
+public interface Consumable {
+
+    /**
+     * Consumes the item and performs its effects on the actor.
+     *
+     * @param actor The actor consuming the item.
+     * @return An integer value representing the effect or change applied to the actor.
+     */
+    int consume(Actor actor);
 }
+
