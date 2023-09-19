ec50a10dcc8ba567512e55c06ed88b2139b15663
dpar0036 <dpar0036@student.monash.edu>
2023-09-19 08:12:22 +0000

    Replace ConsumableItem.java

--- src/game/items/consumables/ConsumableItem.java
+++ src/game/items/consumables/ConsumableItem.java
@@ -1,4 +1,30 @@
 package game.items.consumables;
 
-public class ConsumableItem {
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
+import edu.monash.fit2099.engine.items.Item;
+/**
+ * An abstract class representing a consumable item that can be used by an actor. Extends the Item class.
+ */
+public abstract class ConsumableItem extends Item implements Consumable {
+    BaseActorAttributes modifiedAttribute;
+
+    /**
+     * Constructor for the ConsumableItem class.
+     *
+     * @param name        The name of this item.
+     * @param displayChar The character to use to represent this item if it is on the ground.
+     * @param portable    True if and only if the item can be picked up.
+     */
+    public ConsumableItem(String name, char displayChar, boolean portable) {
+        super(name, displayChar, portable);
+    }
+
+    /**
+     * Gets the modified attribute associated with this consumable item.
+     *
+     * @return The BaseActorAttributes representing the modified attribute.
+     */
+    public BaseActorAttributes getModifiedAttribute() {
+        return this.modifiedAttribute;
+    }
 }
