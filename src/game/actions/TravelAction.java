d3bde80eb3ddfcd3c801f9cc97da4b97438f782d
dpar0036 <dpar0036@student.monash.edu>
2023-09-19 08:08:33 +0000

    Replace TravelAction.java

--- src/game/actions/TravelAction.java
+++ src/game/actions/TravelAction.java
@@ -1,4 +1,34 @@
 package game.actions;
 
-public class TravelAction {
+import edu.monash.fit2099.engine.actions.MoveActorAction;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Location;
+
+/**
+ * A specialized action for an actor to travel to a specified destination on a game map.
+ */
+public class TravelAction extends MoveActorAction {
+    private String destination;
+
+    /**
+     * Constructor for the TravelAction class.
+     *
+     * @param moveToLocation The location to which the actor is traveling.
+     * @param destination    A string describing the destination.
+     */
+    public TravelAction(Location moveToLocation, String destination) {
+        super(moveToLocation, destination);
+        this.destination = destination;
+    }
+
+    /**
+     * Returns a description of the TravelAction for use in menus or UI.
+     *
+     * @param actor The actor for whom the description is generated.
+     * @return A string describing the action for display in menus.
+     */
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + " travels to " + this.destination;
+    }
 }
