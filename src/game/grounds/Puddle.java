cef0f06bb61ccda370f00b889901f2f3ec48e585
dpar0036 <dpar0036@student.monash.edu>
2023-09-19 08:11:15 +0000

    Replace Puddle.java

--- src/game/grounds/Puddle.java
+++ src/game/grounds/Puddle.java
@@ -2,8 +2,15 @@ package game.grounds;
 
 import edu.monash.fit2099.engine.positions.Ground;
 
+/**
+ * Class representing a puddle ground on the game map.
+ */
 public class Puddle extends Ground {
+    /**
+     * Constructor for the Puddle class.
+     */
     public Puddle() {
         super('~');
     }
 }
+
