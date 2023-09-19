6ee685602b545a4874827cf281a1765a5977e786
dpar0036 <dpar0036@student.monash.edu>
2023-09-19 08:11:41 +0000

    Replace Wall.java

--- src/game/grounds/Wall.java
+++ src/game/grounds/Wall.java
@@ -4,17 +4,28 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
 
 /**
+ * Class representing a wall ground on the game map that blocks actor movement.
  * Created by:
  * @author Riordan D. Alfredo
  * Modified by:
- *
+ * Lim Hung Xuan
  */
+
 public class Wall extends Ground {
 
+    /**
+     * Constructor for the Wall class.
+     */
     public Wall() {
         super('#');
     }
 
+    /**
+     * Checks whether an actor can enter the wall, which is always false since walls are impassable.
+     *
+     * @param actor The actor attempting to enter the wall.
+     * @return Always returns false, indicating that actors cannot enter a wall.
+     */
     @Override
     public boolean canActorEnter(Actor actor) {
         return false;
