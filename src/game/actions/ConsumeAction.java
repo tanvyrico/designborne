38fbfe5ed3ba7ff1e736e0a2b3b3e3c1b50bf5ff
dpar0036 <dpar0036@student.monash.edu>
2023-09-19 07:56:13 +0000

    Replace ConsumeAction.java

--- src/game/actions/ConsumeAction.java
+++ src/game/actions/ConsumeAction.java
@@ -1,4 +1,51 @@
 package game.actions;
 
-public class ConsumeAction {
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+
+import game.items.consumables.ConsumableItem;
+
+/**
+ * A class representing an action where an actor consumes a consumable item.
+ * This action allows an actor to consume a consumable item and gain the specified benefits.
+ */
+public class ConsumeAction extends Action {
+    private ConsumableItem consumable;
+
+    /**
+     * Constructor for the ConsumeAction class.
+     *
+     * @param consumable The consumable item that the actor intends to consume.
+     */
+    public ConsumeAction(ConsumableItem consumable) {
+        this.consumable = consumable;
+    }
+
+    /**
+     * Executes the ConsumeAction, allowing the actor to consume the specified consumable item.
+     * After consumption, the consumable item is removed from the actor's inventory.
+     *
+     * @param actor The actor performing the action.
+     * @param map   The GameMap on which the action is performed.
+     * @return A message describing the consumption and its effects.
+     */
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        int buffedPoints = this.consumable.consume(actor);
+        actor.removeItemFromInventory(this.consumable);
+        return actor + " consumes " + this.consumable + " and " + this.consumable + " restores the " +
+                this.consumable.getModifiedAttribute() + " of " + actor + " by " + buffedPoints + " points.";
+    }
+
+    /**
+     * Returns a description of the ConsumeAction for use in menus or UI.
+     *
+     * @param actor The actor for whom the description is generated.
+     * @return A string describing the action for display in menus.
+     */
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + " consumes " + this.consumable;
+    }
 }
