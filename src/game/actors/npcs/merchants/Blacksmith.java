5e03128127838c4bd92b841c779f64fe1dba13db
Enrico011 <enricotanvy@gmail.com>
2023-10-23 18:01:26 +0800

    make Blacksmith implement monologist

--- src/game/actors/npcs/merchants/Blacksmith.java
+++ src/game/actors/npcs/merchants/Blacksmith.java
@@ -6,13 +6,36 @@ import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
+import game.Monologist;
 import game.capabilities.Status;
 
-public class Blacksmith extends Actor{
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Blacksmith extends Actor implements Monologist {
+
+    private ArrayList<String> monologueOptions = new ArrayList<>(Arrays.asList("I used to be an adventurer like you, but then …. Nevermind, let’s get back to smithing.",
+            "It’s dangerous to go alone. Take my creation with you on your adventure!",
+            "Ah, it’s you. Let’s get back to make your weapons stronger.",
+            "Beyond the burial ground, you’ll come across the ancient woods ruled by Abxervyer. Use my creation to slay them and proceed further!"));
+
     public Blacksmith() {
         super("Blacksmith", 'B', 2147483647);
         this.addCapability(Status.UPGRADE_ITEMS_WEAPONS);
         this.addBalance(0);
+    }
+
+    public String generateMonologue(Actor player){
+        if(player.hasCapability(Status.DEFEATED_ABXERVYER)){
+            this.monologueOptions.remove("Beyond the burial ground, you’ll come across the ancient woods ruled by Abxervyer. Use my creation to slay them and proceed further!");
+            this.monologueOptions.add("Somebody once told me that a sacred tree rules the land beyond the ancient woods until this day.");
+        }
+        if(player.hasCapability(Status.HAS_GREAT_KNIFE)){
+            this.monologueOptions.add("Hey now, that’s a weapon from a foreign land that I have not seen for so long. I can upgrade it for you if you wish.");
+        }
+        Random random = new Random();
+        return monologueOptions.get(random.nextInt(monologueOptions.size()));
     }
 
     @Override
